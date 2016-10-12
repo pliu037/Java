ac5b519b7d9a182d5f8a2e9e1078c2bcafccdaaf
pliu037 <pengliu12@gmail.com>
2016-10-11 22:31:39 -0700

    Miscellaneous
    Optimized the steganography app by removing the intermediate bit String

--- Miscellaneous/src/Steganography.java
+++ Miscellaneous/src/Steganography.java
@@ -8,39 +8,20 @@ public class Steganography {
         return (b >> (7 - position) & 1) == 1;
     }
 
-    static String convertToBinary(String message) {
-        StringBuilder sb = new StringBuilder();
-        int count = 0;
-        while (true) {
-            int charPosition = count / 8;
-            if (charPosition == message.length()) {
-                break;
-            }
-            int bitPosition = count % 8;
-            if (isSet((byte) message.charAt(charPosition), bitPosition)) {
-                sb.append(1);
-            } else {
-                sb.append(0);
-            }
-            count++;
-        }
-        return sb.toString();
-    }
-
-    static void hideBinaryMessage(Picture pic, String outPath, String binaryMessage) {
+    static void hideMessage(Picture pic, String outPath, String message) {
         int index = 0;
         for (int y = 0; y < pic.height(); y ++) {
             for (int x = 0; x < pic.width(); x ++) {
-                if (index == binaryMessage.length()) {
+                if (index / 8 == message.length()) {
                     break;
                 }
                 Color c = pic.get(x, y);
                 int[] colors = new int[]{c.getRed(), c.getGreen(), c.getBlue()};
                 for (int i = 0; i < 3; i ++) {
-                    if (index == binaryMessage.length()) {
+                    if (index / 8 == message.length()) {
                         break;
                     }
-                    if (binaryMessage.charAt(index) == '1') {
+                    if (isSet((byte)message.charAt(index/8), index % 8)) {
                         if (colors[i] % 2 == 0) {
                             colors[i] ++;
                         }
@@ -58,41 +39,30 @@ public class Steganography {
         pic.save(outPath);
     }
 
-    static String revealBinaryMessage(String inPath, int length) {
+    static String revealMessage(String inPath, int length) {
         Picture pic = new Picture(inPath);
         StringBuilder sb = new StringBuilder();
+        int index = 0;
+        byte b = 0;
         for (int y = 0; y < pic.height(); y ++) {
             for (int x = 0; x < pic.width(); x ++) {
                 Color c = pic.get(x, y);;
                 int[] colors = new int[]{c.getRed(), c.getGreen(), c.getBlue()};
                 for (int i = 0; i < 3; i ++) {
+                    b = (byte) (b << 1);
                     if (colors[i] % 2 == 1) {
-                        sb.append(1);
-                    } else {
-                        sb.append(0);
+                        b ++;
                     }
-                    if (sb.length() == length) {
+                    index ++;
+                    if (index % 8 == 0) {
+                        sb.append((char) b);
+                    }
+                    if (index == length) {
                         return sb.toString();
                     }
-                }
-            }
-        }
-        return sb.toString();
-    }
 
-    static String convertToString(String binaryMessage) {
-        int index = 0;
-        StringBuilder sb = new StringBuilder();
-        while (index + 8 <= binaryMessage.length()) {
-            int b = 0;
-            for (int i = 0; i < 8; i ++) {
-                b = (byte) (b << 1);
-                if (binaryMessage.charAt(index) == '1') {
-                    b++;
                 }
-                index ++;
             }
-            sb.append((char)b);
         }
         return sb.toString();
     }
@@ -105,11 +75,8 @@ public class Steganography {
                     "Anybody is welcome to try though. I wonder if I could fit a whole book in here... Anyway, " +
                     "whoever finds this is in for a treat!";
             Picture pic = new Picture(args[0]);
-            String binaryMessage = convertToBinary(message);
-            hideBinaryMessage(pic, args[1], binaryMessage);
-            String revealedBinaryMessage = revealBinaryMessage(args[1], binaryMessage.length());
-            System.out.println(convertToString(revealedBinaryMessage));
-            System.out.println(message.length() * 8 + " " + pic.width() * pic.height() * 3);
+            hideMessage(pic, args[1], message);
+            System.out.println(revealMessage(args[1], message.length() * 8));
         } else {
             return;
         }
